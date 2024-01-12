d675ae883b35041cd9380cc26a0b3943bf7926db
Francisco Uncal <franunca@gmail.com>
2024-01-11 21:46:33 -0300

    Agregando proyecto a github

---
+++ Js/script.js
@@ -0,0 +1,87 @@
+
+//Funcion saludar usuario
+// function saludarUsuario() {
+//   const nombre = prompt("Ingrese su nombre por favor");
+//   alert("Bienvenido a la Casa de Cambio DIVISAS.INC " + nombre);
+// }
+// saludarUsuario();
+
+//Solicitar datos de usuario\
+function datosUsuario() {
+  const cantidad = parseFloat(prompt("Ingrese la cantidad a convertir: "));
+  const monedaOrigen = prompt(
+    "Ingrese la moneda de ORIGEN (USD, EUR, JPY o ARS)"
+  ).toUpperCase();//Tome como valida las minusculas-mayusculas
+  const monedaDestino = prompt(
+    "Ingrese la moneda de DESTINO (USD, EUR, JPY o ARS)"
+  ).toUpperCase();//Tome como valida las minusculas-mayusculas
+
+  return {
+    cantidad: cantidad,
+    monedaOrigen: monedaOrigen,
+    monedaDestino: monedaDestino,
+  };
+}
+
+//Funcion para convertir moneda
+function convertirMoneda(datosUsuario) {
+  const cantidad = datosUsuario.cantidad;
+  const monedaOrigen = datosUsuario.monedaOrigen;
+  const monedaDestino = datosUsuario.monedaDestino;
+
+  // Validar con while cantidad
+  while (isNaN(cantidad)) {
+    alert("La cantidad ingresada no es valida.");
+    cantidad = parseFloat(prompt("Ingrese la cantidad a convertir: "));
+  }
+
+  const tasaOrigen = tasasDeCambio.find((tasa) => {
+    return tasa.divisa === monedaOrigen;
+  });
+
+  const tasaDestino = tasasDeCambio.find((tasa) => {
+    return tasa.divisa === monedaDestino;
+  });
+
+  if (tasaOrigen !== undefined && tasaDestino !== undefined) {
+    const cantidadConvertida =
+      cantidad * (tasaDestino.valor / tasaOrigen.valor);
+    return {
+      cantidad: cantidad,
+      monedaOrigen: tasaOrigen.divisa,
+      monedaDestino: tasaDestino.divisa,
+      cantidadConvertida: cantidadConvertida,
+    };
+  } else {
+    alert("Moneda no valida.");
+  }
+}
+
+//Realizar operacion
+const resultado = convertirMoneda(datosUsuario());
+
+alert(
+    resultado.cantidad +
+    resultado.monedaOrigen +
+    " equivale a " +
+    resultado.cantidadConvertida +
+    resultado.monedaDestino
+);
+
+//Consultar si desea realizar otra operacion
+const nuevaOperacion = prompt(
+  "Desea realizar otra operacion? \n Si - (Digite 1) \n No - (Digite 2)"
+);
+if (nuevaOperacion === "1") {
+  const resultado = convertirMoneda(datosUsuario());
+
+  alert(
+      resultado.cantidad +
+      resultado.monedaOrigen +
+      " equivale a " +
+      resultado.cantidadConvertida +
+      resultado.monedaDestino
+  );
+} else {
+  alert("Muchas gracias por utilizar nuestro conversor de divisas.");
+}
---
+++ Js/tasas.js
@@ -0,0 +1,7 @@
+// Definir tasas de cambio en array
+const tasasDeCambio = [
+  { divisa: "USD", valor: 1 }, //Dolar estadounidenese 0
+  { divisa: "EUR", valor: 0.85 }, //Euro 1
+  { divisa: "JPY", valor: 114.26 }, //Yen japones 2
+  { divisa: "ARS", valor: 99.5 }, //Peso Argentino
+];
